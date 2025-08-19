797886d83fdb8e38309b5836503ade05163510c0
Anonymous87123 <haizaigahei@outlook.com>
2025-08-19 15:56:38 +0800

    add Demo1.py

---
+++ Demo1.py
@@ -0,0 +1,34 @@
+height=float(input("请输入您的身高，注意单位是米:"));
+weight=float(input("请输入您的体重，注意单位是千克:"));
+bmi=weight/(height*height)
+if bmi<18.5:
+    print("您的bmi指数为:"+str(bmi))
+    print("体重过轻")
+if bmi>=18.5 and bmi<24.9:
+    print("您的bmi指数为:"+str(bmi))
+    print("体重正常")
+if bmi>=24.9 and bmi<29.9:
+    print("您的bmi指数为:"+str(bmi))
+    print("体重稍重")
+if bmi>=29.9:
+    print("您的bmi指数为:"+str(bmi))
+    print("体重过重")
+
+m=313.5
+n=str(m)
+s=int(m)
+ss=str(s)
+print("商品总金额为"+n+"元")
+print("实际支付金额为"+ss+"元")
+
+x=input("请您为电影打分(只能输入0-10的整数):")
+y=int(x)
+print("好的，您的评分是："+"⭐"*y)
+
+print("打折活动火热进行中...")
+x=input("请用数字回答今天是星期几？你的回答是：")
+y=int(input("请用数字回答现在几点了？(24小时制),你的回答是："))
+if (x==2 and (y>=19 and y<=20)) or (x==3 and (y>=19 and y<=20)):
+    print("恭喜你获得10%的折扣！")
+else:
+    print("折扣活动未开始或已结束，不享受折扣。")
