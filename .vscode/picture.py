20be6232453325b9895718580ad00c8418e7bc8d
Anonymous87123 <haizaigahei@outlook.com>
2025-08-31 23:24:35 +0800

    add pic_import

--- .vscode/picture.py
+++ .vscode/picture.py
@@ -1,2 +1,11 @@
-from pyecharts import Line
-line = Line
+from pyecharts.charts import Line
+from pyecharts.options import TitleOpts
+line = Line()
+line.add_xaxis(["2017-10-24", "2017-10-25", "2017-10-26", "2017-10-27", "2017-10-28", "2017-10-29", "2017-10-30"])
+line.add_yaxis("商家A", [11, 12, 13, 14, 15, 16, 17])
+line.add_yaxis("商家B", [21, 22, 23, 24, 25, 26, 27])
+line.add_yaxis("商家C", [31, 32, 33, 34, 35, 36, 37])
+line.set_global_opts(
+    title_opts=TitleOpts(title="某商场销售数据", subtitle="纯属虚构",pos_left="center", pos_top="1%")
+)
+line.render()
