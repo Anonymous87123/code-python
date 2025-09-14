0aca8bdc9c1f4ed9c6fc14e6230b82fb20a9a000
Anonymous87123 <haizaigahei@outlook.com>
2025-09-14 21:20:07 +0800

    add8

--- T8.py
+++ T8.py
@@ -1,3 +1,12 @@
-permutations = []
-for i in range(1,10):
-    for j in range
+import itertools
+permutations = list(itertools.permutations(range(1, 10)))
+count_permutations= len(permutations)
+print(count_permutations)
+count = 0
+for [i1,i2,i3,i4,i5,i6,i7,i8,i9] in permutations:
+    m = i1*100 + i2*10 + i3*1
+    n = i4*100 + i5*10 + i6*1
+    o = i7*100 + i8*10 + i9*1
+    if m+n+o == 2025:
+        count += 1
+print(count)
